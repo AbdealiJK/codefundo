c1bde41b2d21c7b5df82814aba65dd05724b2913
nishanthkarthik <nishanthkarthik@live.com>
2015-02-28 17:52:59 +0530

    Image Upload added

--- winfinityClient/winfinityClient/createroom.xaml.cs
+++ winfinityClient/winfinityClient/createroom.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Navigation;
 using Coding4Fun.Toolkit.Controls;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 using Newtonsoft.Json;
 using RestSharp;
 using winfinityClient.Helpers;
@@ -22,6 +26,7 @@ namespace winfinityClient
         int _noDevices;
         UserCreate _myID;
         RoomCreate _myRoom;
+        RoomAddUser _myWorkspace;
 
         public createroom()
         {
@@ -45,6 +50,7 @@ namespace winfinityClient
         {
             _myID = (UserCreate)PhoneApplicationService.Current.State["MyID"];
             Device1.Content = _myID.data.key;
+            _deviceIdList[0] = _myID.data.key;
         }
 
         private void TwoDevice_Click(object sender, RoutedEventArgs e)
@@ -74,6 +80,7 @@ namespace winfinityClient
             input2.Completed += input2_Completed;
             input2.Title = "Setup device 2";
             input2.Message = "Enter the UID of device 2";
+            input2.InputScope = new InputScope { Names = { new InputScopeName() { NameValue = InputScopeNameValue.Number } } };
             input2.Show();
         }
 
@@ -92,6 +99,7 @@ namespace winfinityClient
             input3.Completed += input3_Completed;
             input3.Title = "Setup Device 3";
             input3.Message = "Enter the UID of device 3";
+            input3.InputScope = new InputScope { Names = { new InputScopeName() { NameValue = InputScopeNameValue.Number } } };
             input3.Show();
         }
 
@@ -110,6 +118,7 @@ namespace winfinityClient
             input4.Completed += input4_Completed;
             input4.Title = "Setup Device 4";
             input4.Message = "Enter the UID of device 4";
+            input4.InputScope = new InputScope { Names = { new InputScopeName() { NameValue = InputScopeNameValue.Number } } };
             input4.Show();
         }
 
@@ -130,16 +139,75 @@ namespace winfinityClient
                 if (_deviceIdList[i] == string.Empty || _deviceIdList[i] == null)
                     ++nullCounter;
             }
+
             if (nullCounter == 0)
             {
+                //Setup workspace
+                CreateNewRoom();
 
-                //Navigate to image workarea
-                MessageBox.Show("Done");
+
             }
             else
             {
-                MessageBox.Show("Enter Device IDs for all devices");
+                Dispatcher.BeginInvoke(() => { MessageBox.Show("Enter Device IDs for all devices"); });
             }
+        }
+
+        void photoGet_Completed(object sender, PhotoResult e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+                return;
+            }
+            if (e.ChosenPhoto == null)
+            {
+                Dispatcher.BeginInvoke(() => { MessageBox.Show("Null image"); });
+                return;
+            }
+
+            //upload image
+            RestClient client = new RestClient("http://cfi.iitm.ac.in/webops/hackathon/hybriddevs/api/room/");
+            RestRequest request = new RestRequest { Method = Method.POST };
+            request.AddParameter("type", "file", ParameterType.GetOrPost);
+            request.AddParameter("room_id", _myRoom.data.room_id, ParameterType.GetOrPost);
+            request.AddParameter("key", _myID.data.key, ParameterType.GetOrPost);
+            request.AddFile("file", e.ChosenPhoto.ToByteArray(), e.OriginalFileName);
+            try
+            {
+                client.ExecuteAsync(request, response =>
+                {
+                    if (response.ResponseStatus == ResponseStatus.Completed)
+                    {
+                        Dispatcher.BeginInvoke(() =>
+                        {
+                            ToastPrompt toast = new ToastPrompt
+                            {
+                                Message = "Image Added",
+                                MillisecondsUntilHidden = 1000
+                            };
+                            toast.Show();
+                            if (SystemTray.ProgressIndicator != null)
+                                SystemTray.ProgressIndicator.IsVisible = false;
+                            SystemTray.IsVisible = false;
+                        });
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //Show SysTray
+            SystemTray.ProgressIndicator = new ProgressIndicator();
+            SystemTray.ProgressIndicator.IsIndeterminate = true;
+            SystemTray.ProgressIndicator.Text = "Uploading Image";
+            SystemTray.ProgressIndicator.IsVisible = true;
+            SystemTray.IsVisible = true;
+
+            //Navigate to image workarea
+            MessageBox.Show("Please wait until the image is done uploading");
         }
 
         private void CreateNewRoom()
@@ -160,12 +228,23 @@ namespace winfinityClient
                         {
                             RoomTitle.Text = "room id " + _myRoom.data.room_id.ToString();
                         });
+                        switch (_noDevices)
+                        {
+                            case 2:
+                                AddUserToRoom((string)Device2.Content, _myRoom, 1);
+                                break;
+                            case 4:
+                                AddUserToRoom((string)Device2.Content, _myRoom, 1);
+                                AddUserToRoom((string)Device3.Content, _myRoom, 2);
+                                AddUserToRoom((string)Device4.Content, _myRoom, 3);
+                                break;
+                        }
                     }
                 });
             }
             catch (Exception e)
             {
-                MessageBox.Show(e.Message);
+                Dispatcher.BeginInvoke(() => { MessageBox.Show(e.Message); });
             }
         }
 
@@ -186,11 +265,14 @@ namespace winfinityClient
                     {
                         Dispatcher.BeginInvoke(() =>
                             {
-                                ToastPrompt toast = new ToastPrompt();
-                                toast.Message = "User added";
-                                toast.MillisecondsUntilHidden = 1000;
+                                ToastPrompt toast = new ToastPrompt
+                                {
+                                    Message = "User added",
+                                    MillisecondsUntilHidden = 2000
+                                };
                                 toast.Show();
                             });
+                        _myWorkspace = JsonConvert.DeserializeObject<RoomAddUser>(response.Content);
                     }
                 });
             }
@@ -198,6 +280,14 @@ namespace winfinityClient
             {
                 MessageBox.Show(e.Message);
             }
+        }
+
+        private void ImageUpload_Click(object sender, RoutedEventArgs e)
+        {
+            PhotoChooserTask photoGet = new PhotoChooserTask();
+            photoGet.ShowCamera = true;
+            photoGet.Completed += photoGet_Completed;
+            photoGet.Show();
         }
     }
 }
