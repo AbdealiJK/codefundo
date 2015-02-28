d0a0a29b883c95c8860c344093c78586d2e80945
AbdealiJK <abdealikothari@gmail.com>
2015-02-28 07:58:38 +0530

    web: Install base modules
    Install ipython, django rest framework, django extensions to make life easier

--- web/web/globalsettings.py
+++ web/web/globalsettings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +83,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Always use IPython for shell_plus
+SHELL_PLUS = "ipython"
